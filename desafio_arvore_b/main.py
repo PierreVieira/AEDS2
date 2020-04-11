5dc0291184b1aac54b455499dada8791dc8cc415
Pierre Vieira <pierrevieiraggg@gmail.com>
2020-04-10 22:32:15 -0300

    [+] Módulo de teste unitário

--- desafio_arvore_b/main.py
+++ desafio_arvore_b/main.py
@@ -7,27 +7,4 @@ from random import shuffle, randint
 from desafio_arvore_b.tree_b.arvore_b import Tree_b
 
 
-def teste_de_insercao(qtde_maxima_elementos_por_pagina, raiz, valores):
-    arvore_b = Tree_b(qtde_maxima_elementos_por_pagina, raiz)
-    for value in valores:
-        arvore_b.insert(value)
-    return arvore_b
 
-
-def mensagem_de_busca(arvore: Tree_b, value):
-    if arvore.find(value):
-        print(f'O valor {value} foi encontrado na árvore.')
-    else:
-        print(f'O valor {value} não foi encontrado na árvore.')
-
-
-def teste_de_busca(arvore, valores_a_serem_pesquisados):
-    for value in valores_a_serem_pesquisados:
-        mensagem_de_busca(arvore, value)
-
-
-arvore1 = teste_de_insercao(2, 11, [8, 11, 27, 31, 25, 16, 59, 53, 52, 21, 36, 48, 78, 81, 75, 95, 90, 91, 72, 63, 20, 35])
-print(arvore1.conteudo())
-print('\n\n')
-arvore2 = teste_de_insercao(2, 20, [20, 10, 40, 50, 30, 55, 3, 11, 4, 28, 36, 33, 52, 17, 25, 13, 45, 9, 43, 8, 48])
-print(arvore2.conteudo())
---
+++ desafio_arvore_b/tree_b_test.py
@@ -0,0 +1,14 @@
+import unittest
+from desafio_arvore_b.tree_b.arvore_b import Tree_b
+
+
+class Tree_b_Test(unittest.TestCase):
+    def arvore_com_elementos(self, qtde_maxima_elementos_por_pagina, raiz, valores):
+        arvore_b = Tree_b(qtde_maxima_elementos_por_pagina, raiz)
+        for value in valores:
+            arvore_b.insert(value)
+        return arvore_b
+
+    def test_insert(self):
+        arvore1 = self.arvore_com_elementos(2, 11, [8, 11, 27, 31, 25, 16, 59, 53, 52, 21, 36, 48, 78, 81, 75, 95, 90, 91, 72, 63, 20, 35])
+        arvore2 = self.arvore_com_elementos(2, 20, [20, 10, 40, 50, 30, 55, 3, 11, 4, 28, 36, 33, 52, 17, 25, 13, 45, 9, 43, 8, 48])
