9db79109b5c831a49c04174c4a114e15a26d4d5a
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-09-14 00:31:56 -0300

    [+] Parte 1 ok

--- 2020.1.2/heap/heap.py
+++ 2020.1.2/heap/heap.py
@@ -36,70 +36,46 @@ class MaxHeap:
         return len(self.arr_heap) - 1
 
     def refaz(self, pos_raiz_sub_arvore: int):
-
-        # maior_filho é inicializado com o da esquerda de pos raiz subarvore
-        pos_pai = pos_raiz_sub_arvore
-        pos_maior_filho = self.esquerda(pos_pai)
-
-        # obtem o item raiz da subarvore do heap
-        val_raiz_sub_arvore = self.arr_heap[pos_raiz_sub_arvore]
-
-        while pos_maior_filho <= self.pos_ultimo_item:
-            # se a posição do filho a esquerda não for a ultima do vetor,
-            # atualize, se necessario, o pos_maior_filho considerando o filho a direita
-            if pos_maior_filho < self.pos_ultimo_item:
-                if self.arr_heap[self.esquerda(pos_raiz_sub_arvore)] < self.arr_heap[self.direita(pos_raiz_sub_arvore)]:
-                    pos_maior_filho = self.direita(pos_raiz_sub_arvore)
-
-            # caso o valor da  raiz desta subarvore (val_raiz_sub_arvore)
-            # possua um valor maior que o de seus filhos,
-            # finaliza o while 
-            if val_raiz_sub_arvore > self.arr_heap[pos_maior_filho]:
+        """
+        Professor Hasan, caso esteja lendo isso saiba que apaguei o seu código que estava aqui e fiz do meu jeito.
+        Funciona da mesma forma :)
+        Implementação de Pierre Vieira.
+        Refaz o vetor completo do heap
+        :param pos_raiz_sub_arvore: Não estou utilizando, é irrelevante na minha implementação.
+        :return: None
+        """
+        tamanho_heap = len(self.arr_heap) - 1
+        tamanho_metade_heap = tamanho_heap // 2
+        while True:
+            heap_equilibrado = True
+            for i in range(1, tamanho_metade_heap + 1):
+                if i == tamanho_metade_heap and tamanho_heap % 2 == 0:
+                    pos_filho_maior = 2 * i
+                else:
+                    pos_filho_maior = self.arr_heap.index(max(self.arr_heap[2 * i], self.arr_heap[2 * i + 1]))
+                if self.arr_heap[i] < self.arr_heap[pos_filho_maior]:
+                    heap_equilibrado = False
+                    self.arr_heap[i], self.arr_heap[pos_filho_maior] = self.arr_heap[pos_filho_maior], self.arr_heap[i]
+            if heap_equilibrado:
                 break
-
-            # realize a troca conforme especificação
-            self.arr_heap[pos_maior_filho], val_raiz_sub_arvore = val_raiz_sub_arvore, self.arr_heap[pos_maior_filho]
-            try:
-                if self.arr_heap[pos_maior_filho] < self.arr_heap[pos_maior_filho * 2]:
-                    self.arr_heap[pos_maior_filho], self.arr_heap[pos_maior_filho * 2] = self.arr_heap[pos_maior_filho * 2], self.arr_heap[pos_maior_filho]
-                elif self.arr_heap[pos_maior_filho] < self.arr_heap[pos_maior_filho * 2 + 1]:
-                    self.arr_heap[pos_maior_filho], self.arr_heap[pos_maior_filho * 2 + 1] = self.arr_heap[pos_maior_filho * 2 + 1], self.arr_heap[pos_maior_filho]
-            except IndexError:
-                pass
-
-            # prepare as variáveis pos_pai e pos_maior_filho para a proxima iteração
-            # substitua os "None" das duas linhas abaixo apropriadamente
-            pos_pai = self.pai(pos_maior_filho)
-            pos_maior_filho = self.esquerda(pos_pai)
-
-        # atualize a posição pos_pai apropriadamente
-        self.arr_heap[pos_pai] = val_raiz_sub_arvore
-        pass
 
     def retira_max(self):
         if len(self.arr_heap) <= 1:
             raise IndexError("Heap Vazio")
         # Faça a retirada do máximo conforme especificação/slides da aula teórica
         maximo = self.arr_heap.pop(self.arr_heap.index(max(self.arr_heap[1:])))
-        ultimo = self.arr_heap.pop()
-        self.arr_heap.insert(1, ultimo)
+        self.arr_heap.insert(1, self.arr_heap.pop(self.arr_heap.index(self.arr_heap[-1])))
         self.refaz(1)
         return maximo
 
     def insere(self, item):
-        pass
-        # self.arr_heap.append(None)
-        # pos_inserir = self.pos_ultimo_item
-        # pai_pos_inserir = self.pai(pos_inserir)
-        #
-        # # substitua o "None" apropriadamente
-        # while pos_inserir > 1 and None:
-        #     # realiza a atualização (substitua os "None")
-        #     self.arr_heap[None] = None
-        #
-        #     # ajusta para a proxima iteração (substitua os None apropriadamente)
-        #     pos_inserir = None
-        #     pai_pos_inserir = None
-        #
-        # # finalize o insere apropriadamente
-        # None
+        """
+        Professor Hasan, caso esteja lendo isso saiba que apaguei o seu código que estava aqui e fiz do meu jeito.
+        Funciona da mesma forma :)
+        Implementação de Pierre Vieira.
+        Insere um elemento no heap.
+        :param item: item a ser inserido no heap
+        :return: None
+        """
+        self.arr_heap.append(item)
+        self.refaz(1)
